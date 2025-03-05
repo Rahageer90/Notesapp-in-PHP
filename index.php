1f7812dcc9fff3186e547a1032f4ed160faf4d01
Rahageer Saadman Islam <130089381+Rahageer90@users.noreply.github.com>
2025-03-06 01:27:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,8 @@ $config = require 'config.php';
 $dbConfig = $config['database'];
 $db = new Database($dbConfig);
 require 'route.php';
+
+<?php
+echo "<pre>";
+print_r(scandir(__DIR__));
+echo "</pre>";
