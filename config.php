8580c43ad46fea2533abdc081fd3686f1974fe1a
Rahageer Saadman Islam <130089381+Rahageer90@users.noreply.github.com>
2025-03-06 00:28:20 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-return[
+return [
     'database' => [
-        'host' => 'localhost',
-        'port'=> 3306,
-        'dbname' => 'notes',
-        'user' => 'root',
-        'password' => '',
+        'host' => getenv('DB_HOST'),
+        'port' => getenv('DB_PORT'),
+        'dbname' => getenv('DB_NAME'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
         'charset' => 'utf8mb4'
-    ],
-    ];
+    ]
+];
