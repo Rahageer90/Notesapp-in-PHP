11f6c4f640aeb2d6f9fb5e73c504a551e20bebae
Rahageer Saadman Islam <130089381+Rahageer90@users.noreply.github.com>
2025-03-06 02:07:38 +0600

    Update register.php

--- controllers/register.php
+++ controllers/register.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/../database.php';
-require_once __DIR__.'/../models/user.php';
+require_once __DIR__.'/../models/User.php';
 class RegisterController{
     private $pdo;
     public function __construct(){
