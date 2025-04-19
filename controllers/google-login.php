371f434bd5eec1d7385664cab681f3115e6c7dc5
Rahageer Saadman Islam <rahageer.saadman.islam@g.bracu.ac.bd>
2025-04-19 18:47:18 +0600

    bug fixes

--- controllers/google-login.php
+++ controllers/google-login.php
@@ -1,11 +1,14 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../database.php';
 require_once __DIR__ . '/../models/User.php';
 
+use Google\Client as GoogleClient;
+
 session_start();
 
-$client = new Google_Client();
+$client = new GoogleClient();
 $client->setClientId('722234426503-ckp8udm4am8dlj9svb876b4gtld7l1f7.apps.googleusercontent.com');
 $client->setClientSecret('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');
 $client->setRedirectUri('https://your-app.onrender.com/google-callback');
@@ -13,5 +16,7 @@ $client->addScope('email');
 $client->addScope('profile');
 
 $authUrl = $client->createAuthUrl();
+
+// Use 302 redirect with sanitization
 header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
 exit;
