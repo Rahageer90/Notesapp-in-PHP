c03dbebab53cdac83e77cbb671c4f8b2c93f30a8
Rahageer Saadman Islam <rahageer.saadman.islam@g.bracu.ac.bd>
2025-04-19 22:41:27 +0600

    bug fix

--- controllers/google-callback.php
+++ controllers/google-callback.php
@@ -13,7 +13,7 @@ session_start();
 $client = new GoogleClient();
 $client->setClientId('722234426503-ckp8udm4am8dlj9svb876b4gtld7l1f7.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('https://your-app.onrender.com/google-callback');
+$client->setRedirectUri('https://notesapp-in-php.onrender.com//google-callback');
 $client->addScope('email');
 $client->addScope('profile');
 
