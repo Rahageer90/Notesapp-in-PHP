ff7e4d0ad15939cecac7f6ba2314ae0591a4226c
Rahageer Saadman Islam <rahageer.saadman.islam@g.bracu.ac.bd>
2025-04-19 18:31:49 +0600

    change

--- views/index.view.php
+++ views/index.view.php
@@ -6,7 +6,7 @@
         <p class="mt-4 text-lg text-gray-600">Here you can create your notes and use the to-do list</p>
         
         <div class="mt-8 flex justify-center">
-            <a href="/google-login" class="w-full sm:w-auto">
+            <a href="/login" class="w-full sm:w-auto">
                 <button class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                     Login with Google
                 </button>
